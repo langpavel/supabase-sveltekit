f187196a46af33b5c184207c1af3586b8eafea23
pala <pala@tmapy.cz>
2022-12-20 15:35:53 +0100

    wip

---
+++ src/lib/supabaseClient.ts
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/auth-helpers-sveltekit';
+import { env } from '$env/dynamic/public';
+
+export const supabase = createClient(env.PUBLIC_SUPABASE_URL!, env.PUBLIC_SUPABASE_ANON_KEY!);
+
+if (typeof window !== 'undefined') {
+  window.SUPA = supabase;
+}
